5c9b0b1486c3314ea8a313c69510f11f20d59431
Zubin Dodhi <zubindodhi@Zubins-MacBook-Pro.local>
2017-10-09 21:58:54 -1000

    ex3a

--- exercises.js
+++ exercises.js
@@ -50,10 +50,17 @@ var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe']
 	console.log("The value at " + i + " is " + presidents[i] )
 }
 
+function leaders(person) {
+	for (var i = 0; i < person.length; i++) {
+	console.log("President " + person[i] +" was a great leader." ) 
+}
+}
+leaders(presidents);
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
+var stringOfNumbers = ""
 
 
 /* 5) Even Stevens
